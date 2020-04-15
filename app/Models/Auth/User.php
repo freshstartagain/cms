7aa936ab9583552e5563634e2e3cec688df20e14
freshstartagain <newstartagain@gmail.com>
2020-04-15 17:57:12 +0800

    User migration and seed

--- app/Models/Auth/User.php
+++ app/Models/Auth/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Auth;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -9,7 +10,27 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable implements JWTSubject
 {
-    use Notifiable;
+    use Notifiable, SoftDeletes;
+
+    protected $table = "user";
+
+    protected $fillable = [
+            'firstname', 
+            'middlename', 
+            'lastname', 
+            'email', 
+            'username', 
+            'password', 
+            'mobile',
+            'address_id',
+            'position_id',
+    ];
+
+    protected $hidden = [
+        'password', 
+        'remember_token',
+    ];
+
 
 
     public function getJWTIdentifier()
