710889d42a7a7711028dc125b49a90d19feda400
freshstartagain <newstartagain@gmail.com>
2020-04-15 16:22:27 +0800

    Installed jwt

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,20 +2,26 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
+use JWTAuth;
+use Exception;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
 {
-    /**
-     * Get the path the user should be redirected to when they are not authenticated.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return string|null
-     */
-    protected function redirectTo($request)
+    public function handle($request, Closure $next)
     {
-        if (! $request->expectsJson()) {
-            return route('login');
+        try {
+            $user = JWTAuth::parseToken()->authenticate();
+        } catch (Exception $e) {
+            if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException) {
+                return response()->json(['status' => 'Token is Invalid'], 400);
+            } else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException) {
+                return response()->json(['status' => 'Token is Expired'], 400);
+            } else {
+                return response()->json(['status' => 'Authorization Token not found'], 400);
+            }
         }
+        return $next($request);
     }
 }
---
+++ app/Models/Auth/User.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models\Auth;
+
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+
+class User extends Authenticatable implements JWTSubject
+{
+    use Notifiable;
+
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+}
