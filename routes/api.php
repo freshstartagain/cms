64e10f4e63e70f8d12577b64fad52ac88729095f
freshstartagain <newstartagain@gmail.com>
2020-04-20 17:38:35 +0800

    Done with the initial routes for address

--- routes/api.php
+++ routes/api.php
@@ -9,9 +9,27 @@ Route::prefix('v1')->group(function(){
         Route::post('login', 'Auth\UserController@login');
 
         Route::middleware('auth')->group(function(){
+            // User Auth
             Route::get('user', 'Auth\UserController@show');
             Route::post('logout', 'Auth\UserController@logout');
             Route::post('refresh', 'Auth\UserController@refresh');
         });
     });
+
+    Route::middleware('auth')->group(function(){
+        Route::get('street','Address\AddressController@street');
+        Route::get('barangay','Address\AddressController@barangay');
+        Route::get('municipal','Address\AddressController@municipal');
+        Route::get('province','Address\AddressController@province');
+        Route::get('region','Address\AddressController@region');
+
+        Route::prefix('user')->group(function(){
+            // Address
+            Route::get('{id}/address', 'Address\UserAddressController@index');
+            Route::post('{id}/address', 'Address\UserAddressController@index');
+            Route::put('{id}/address', 'Address\UserAddressController@index');
+            Route::delete('{id}/address', 'Address\UserAddressController@index');
+        });
+    });
+    
 });
